b1795e62e8c7a1e1f84e49271f90a5355e4d8000
kusi <git@titlis.org>
2023-04-20 22:33:50 +0200

    new branch for rauth fork
    new branch contains
    - fix binary data issue
    - fix for changing dict during iteration

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Markus Leuthold",
     author_email="github@titlis.org",
     packages=find_packages(),
-    install_requires=["iso8601", "wheel", "mysqlclient", "requests", "requests", "etaprogress", "pyyaml", "rauth @ git+https://github.com/githubkusi/rauth.git@binarydata"],
+    install_requires=["iso8601", "wheel", "mysqlclient", "requests", "requests", "etaprogress", "pyyaml", "rauth @ git+https://github.com/githubkusi/rauth.git@digikam2smugmug"],
     url="https://github.com/adhawkins/smugmugv2py",
     license="GPL",
     description="Upload Digikam photos and metadata to Smugmug",
